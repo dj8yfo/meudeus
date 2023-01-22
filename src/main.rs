d314289041425a8e0a98544520e1df24fe58aafe
dj8yf0μl <sffhox@gmail.com>
2023-01-22 17:39:33 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-use crate::common::*;
-
-#[cfg(test)]
-#[macro_use]
-mod test_utils;
-
-mod common;
-mod config;
-mod directory;
-mod error;
-mod handler;
-mod matter;
-mod note;
-mod note_id;
-mod opt;
-mod path_ext;
-mod search;
-
 fn main() {
-  match Opt::from_args().run() {
-    Ok(()) => {}
-    Err(e) => {
-      eprintln!("{}: {}", "error".red(), e);
-      process::exit(1);
-    }
-  }
+    println!("Hello, world!");
 }
